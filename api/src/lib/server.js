23eedb7a9dad56711a60f63a5c7425dbe965e1ee
Alican Basak <alicanbasak@icloud.com>
2024-05-21 23:25:15 +0300

    creating env

--- api/src/lib/server.js
+++ api/src/lib/server.js
@@ -2,10 +2,11 @@ import express from "express";
 
 export const startServer = () => {
   const httpServer = express();
+  const port = process.env.PORT;
 
   try {
-    httpServer.listen(3000, () => {
-      console.log("Server is running on port 3000");
+    httpServer.listen(port, () => {
+      console.log(`Server is running on port: ${port}`);
     });
   } catch (err) {
     throw new Error("Server failed to start", err);
